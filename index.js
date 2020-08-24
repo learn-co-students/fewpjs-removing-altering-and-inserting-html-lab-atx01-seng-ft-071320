f47087ffeb24b98d28cd3577dc1cf86257de68d3
allenorton <norton.allene@gmail.com>
2020-08-24 11:56:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.querySelector('main#main')
+main.remove()
+
+const head = document.createElement('h1')
+head.id = "victory"
+document.body.appendChild(head)
+const newHeader = document.querySelector('h1#victory')
+newHeader.innerHTML = 'Allene is the champion'
