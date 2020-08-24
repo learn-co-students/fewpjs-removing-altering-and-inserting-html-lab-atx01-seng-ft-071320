b4a8b8463fc8fdffcb02ace0763e93489d5ce23b
jtskidmore <johntoddskidmore@gmail.com>
2020-08-24 15:39:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+document.getElementById('main').remove();
+
+const h1 = document.createElement('h1');
+
+h1.id = "victory"
+
+document.body.appendChild(h1);
+
+const newHeader = document.getElementById('victory');
+
+newHeader.innerHTML = "JT is the champion!"
