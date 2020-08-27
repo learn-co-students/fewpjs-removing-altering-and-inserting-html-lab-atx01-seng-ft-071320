8f3a7fdceb9ab0b225b357263256dcd4e9a98235
R-Tomas-Gonzalez <tomas@rtomasgonzalez.com>
2020-08-26 21:05:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const main = document.getElementById('main');
+// console.log(main)
+main.remove()
+
+const newHeader = document.createElement('h1');
+// console.log(newHeader)
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Tomas is the champion';
+ console.log(newHeader)
